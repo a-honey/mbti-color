17ca5b03c856513e5a9e84e0babb821344049677
a-honey <jungahyeon0512@khu.ac.kr>
2023-04-17 15:56:20 +0900

    Fix Error: delete package.json, node_modules and install them again

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import mock from "../mock.json";
+import { useState } from "react";
+import filterdeleteimg from "../images/x.svg";
+
+function Home() {
+  const [filter, setFilter] = useState();
+
+  return (
+    <div className="home">
+      <h1>MBTI별 좋아하는 컬러</h1>
+      {filter && (
+        <div onClick={() => setFilter(null)}>
+          {filter}
+          <img src={filterdeleteimg} alt="필터 삭제" />
+        </div>
+      )}
+      <div>
+        <Link to="/new">+ 새 컬러등록하기</Link>
+      </div>
+      <ul>
+        {mock.map((item) => (
+          <li key={item.id} onClick={() => setFilter(item.mbti)}>
+            {item.id} {item.mbti} --- {item.colorCode}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/New.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import repeatimg from "../images/repeat.svg";
+import cancelimg from "../images/x.svg";
+
+function New() {
+  return (
+    <div>
+      <h1>새 컬러 등록하기</h1>
+      <Link to="/">
+        <img src={cancelimg} alt="취소" />
+      </Link>
+      <h2>MBTI</h2>
+      <h2>컬러</h2>
+      <img src={repeatimg} alt="랜덤" />
+      <input name="colorCode" />
+      <button>컬러등록</button>
+    </div>
+  );
+}
+
+export default New;
