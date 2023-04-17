68d39978fcb9fdb423d121be396c75b00cc4e54d
a-honey <jungahyeon0512@khu.ac.kr>
2023-04-17 14:32:36 +0900

    Fixed: eslint Error

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <h1>MBTI별 좋아하는 컬러</h1>
+      <div>
+        <Link to="/new">+ 새 컬러등록하기</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
