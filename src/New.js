c2103c5e12033caa41ff3af90ce39adf7684bc5d
a-honey <jungahyeon0512@khu.ac.kr>
2023-04-18 23:43:47 +0900

    Add MBTI selecting features

--- src/New.js
+++ src/New.js
@@ -1,19 +1,57 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import repeatimg from "./images/repeat.svg";
-import cancelimg from "./images/x.svg";
+import MBTISelect from "./components/MBTIselect";
+import generateColorCode from "./lib/generateColorCode";
 
 function New() {
+  const [formValue, setFormValue] = useState({
+    mbti: "INFP",
+    colorCode: "#000000",
+  });
+
+  function handleChange(name, value) {
+    setFormValue((prevFormValue) => ({
+      ...prevFormValue,
+      [name]: value,
+    }));
+  }
+
+  function handleRandomClick() {
+    const nextColorCode = generateColorCode();
+    handleChange("colorCode", nextColorCode);
+  }
+
+  function handleColorCodeBlur() {
+    const isValidColorCode = /^#[a-fA-F0-9]{6}$/.test(formValue.colorCode);
+    if (!isValidColorCode) {
+      handleChange("colorCode", "#000000");
+    }
+  }
+
+  function handleSubmit() {
+    /** @FIXME */
+    console.log(formValue);
+  }
+
   return (
     <div>
       <h1>새 컬러 등록하기</h1>
       <Link to="/">
-        <img src={cancelimg} alt="취소" />
+        <img src="/images/x.svg" alt="취소" />
       </Link>
       <h2>MBTI</h2>
+      <MBTISelect
+        value={formValue.mbti}
+        onChange={(newMbti) => handleChange("mbti", newMbti)}
+      />
       <h2>컬러</h2>
-      <img src={repeatimg} alt="랜덤" />
-      <input name="colorCode" />
-      <button>컬러등록</button>
+      <img src="/images/repeat.svg" alt="랜덤" onClick={handleRandomClick} />
+      <input
+        value={formValue.colorCode}
+        onBlur={handleColorCodeBlur}
+        onChange={(e) => handleChange("colorCode", e.target.value)}
+      />
+      <button onClick={handleSubmit}>컬러 등록</button>
     </div>
   );
 }
