f8151ea215d31834c956753c7155f5797884647d
a-honey <jungahyeon0512@khu.ac.kr>
2023-04-19 15:15:44 +0900

    Refactored New.js for CSS file

--- src/New.js
+++ src/New.js
@@ -5,6 +5,7 @@ import repeatimg from "./images/repeat.svg";
 import cancelimg from "./images/x.svg";
 import generateColorCode from "./lib/generateColorCode";
 import ColorInput from "./components/ColorInput";
+import styles from "./New.module.css";
 
 function New() {
   const [formValue, setFormValue] = useState({
@@ -24,24 +25,40 @@ function New() {
     handleChange("colorCode", nextColorCode);
   }
 
+  function handleSubmit() {
+    console.log("submit!");
+  }
+
   return (
-    <div>
-      <h1>새 컬러 등록하기</h1>
-      <Link to="/">
-        <img src={cancelimg} alt="취소" />
-      </Link>
-      <h2>MBTI</h2>
-      <MBTIselect
-        value={formValue.mbti}
-        onChange={(newMbti) => handleChange("mbti", newMbti)}
-      />
-      <h2>컬러</h2>
-      <img src={repeatimg} alt="랜덤" onClick={handleRandomClick} />
-      <ColorInput
-        value={formValue.colorCode}
-        onChange={(newColorCode) => handleChange("colorCode", newColorCode)}
-      />
-      <button>컬러등록</button>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.headerHeading}>새 컬러 등록하기</h1>
+        <Link className={styles.cancel} to="/">
+          <img className={styles.cancelIcon} src={cancelimg} alt="취소" />
+        </Link>
+      </header>
+      <section className={styles.section}>
+        <h2>MBTI</h2>
+        <MBTIselect
+          value={formValue.mbti}
+          onChange={(newMbti) => handleChange("mbti", newMbti)}
+        />
+      </section>
+      <section className={styles.section}>
+        <h2 className={styles.sectionHeading}>
+          컬러
+          <button className={styles.random} onClick={handleRandomClick}>
+            <img src={repeatimg} alt="랜덤" onClick={handleRandomClick} />
+          </button>
+        </h2>
+        <ColorInput
+          value={formValue.colorCode}
+          onChange={(newColorCode) => handleChange("colorCode", newColorCode)}
+        />
+      </section>
+      <button className={styles.submit} onClick={handleSubmit}>
+        컬러등록
+      </button>
     </div>
   );
 }
