e3dda02aa35cd16f5302b62b82d7efbe1917aa1b
a-honey <jungahyeon0512@khu.ac.kr>
2023-04-19 15:05:00 +0900

    Refactored Home.js for CSS file

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,42 @@ import mock from "./mock.json";
 import { useState } from "react";
 import filterdeleteimg from "./images/x.svg";
 import ColorSurvey from "./components/ColorSurvey";
+import styles from "./Home.module.css";
 
 function Home() {
   const [filter, setFilter] = useState();
 
   return (
-    <div className="home">
-      <h1>MBTI별 좋아하는 컬러</h1>
-      {filter && (
-        <div onClick={() => setFilter(null)}>
-          {filter}
-          <img src={filterdeleteimg} alt="필터 삭제" />
-        </div>
-      )}
-      <div>
-        <Link to="/new">+ 새 컬러등록하기</Link>
+    <div className={styles.contaner}>
+      <div className={styles.headerContainer}>
+        <header className={styles.header}>
+          <h1 className={styles.heading}>
+            MBTI별
+            <br />
+            <span className={styles.account}>좋아하는 컬러</span>
+          </h1>
+          <div>
+            {filter && (
+              <div onClick={() => setFilter(null)}>
+                {filter}
+                <img src={filterdeleteimg} alt="필터 삭제" />
+              </div>
+            )}
+          </div>
+        </header>
       </div>
-      <ul>
-        {mock.map((item) => (
-          <li key={item.id} onClick={() => setFilter(item.mbti)}>
-            <ColorSurvey value={item} onClick={() => setFilter(item.mbti)} />
-          </li>
-        ))}
-      </ul>
+      <main className={styles.content}>
+        <Link className={styles.addItem} to="/new">
+          + 새 컬러등록하기
+        </Link>
+        <ul className={styles.items}>
+          {mock.map((item) => (
+            <li key={item.id} onClick={() => setFilter(item.mbti)}>
+              <ColorSurvey value={item} onClick={() => setFilter(item.mbti)} />
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
